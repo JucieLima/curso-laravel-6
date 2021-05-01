c4fe04a27702a9b010a2e63afbdd143757224e0a
JucieLima <jucielima@gmail.com>
2021-05-01 19:09:11 -0300

    "Conclusão do módulo 5"

---
+++ app/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'slug'
+    ];
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
---
+++ app/Store.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Store extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'phone',
+        'mobile_phone',
+        'slug'
+    ];
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
